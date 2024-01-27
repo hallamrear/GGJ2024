f52a7533e666ec5810b4dfadeed261b00907042d
Angharad Hill <h017026h@student.staffs.ac.uk>
2024-01-27 00:19:26 +0000

    Player Movement
    + generic wasd movement and a test sprite
    + folder for sprites
    + folder for player scripts

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 0;
+    public Rigidbody2D rigidBody;
+    private Vector2 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Inputs
+        ProcessInputs();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void FixedUpdate()
+    {
+        //Physics
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void Move()
+    {
+        rigidBody.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
